e1f39ccaf02dfeb83a2ffce6d9d444cf755ff72c
Kayla Bragg <69012699+KaylaBragg@users.noreply.github.com>
2025-04-30 22:05:06 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,7 +64,7 @@ export default function Home() {
 }
 
 const generateImage = async (prompt) => {
-  const response = await fetch("http://localhost:3000/generate", {
+  const response = await fetch("/api/generate")
     method: "POST",
     headers: {
       "Content-Type": "application/json"
